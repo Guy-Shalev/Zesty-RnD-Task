a387805d94afe860378199c866bc17b8fa2fce07
Guy-Shalev <guy.shalev.email@gmail.com>
2023-06-10 09:19:00 +0300

    Initial commit

---
+++ src/main/java/com/guyshalev/ZestyRnDTask/ZestyRnDTaskApplication.java
@@ -0,0 +1,13 @@
+package com.guyshalev.ZestyRnDTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZestyRnDTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZestyRnDTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guyshalev/ZestyRnDTask/ZestyRnDTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guyshalev.ZestyRnDTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZestyRnDTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
